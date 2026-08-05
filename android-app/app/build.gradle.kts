3e5a551cd6929d48f78263e6a4429dcff27e631c
ryuuflores2006-crypto <ryuuflores2006-crypto@users.noreply.github.com>
2026-08-05 18:22:46 +0800

    Bump version to 1.10 and provide new APK

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ryuuflores2006.inventorysystem"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.9"
+        versionCode = 11
+        versionName = "1.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -84,3 +84,4 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
+
